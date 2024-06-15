98507da46d522915e240155397a75f0b41150496
Alex Marmolejo <42818181+prosperitty@users.noreply.github.com>
2024-06-14 21:16:53 -0400

    updated links due to username change

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -27,7 +27,7 @@ function Projects(props) {
         title='Wreathe'
         alt='wreathe app preview'
         description='A social media app for investors. Built with full-stack modern technologies. Functionality is similar to twitter. Includes search, instant messaging, profile creation, and more.'
-        githubURL='https://github.com/alex-lvl/wreathe'
+        githubURL='https://github.com/prosperitty/wreathe'
         demoURL='https://wreathe.vercel.app/'
       >
         <Skill
@@ -77,7 +77,7 @@ function Projects(props) {
         title='Personal Blog'
         alt='next.js personal blog preview'
         description='This project was built with the next.js tutorial guide which also utilizes the external api endpoint from my blog api project to retrieve blog posts authored by me'
-        githubURL='https://github.com/alex-lvl/nextjs-blog'
+        githubURL='https://github.com/prosperitty/nextjs-blog'
         demoURL='https://nextjs-blog-git-main-alex-lvl.vercel.app/'
       >
         <Skill
@@ -106,8 +106,8 @@ function Projects(props) {
         articles, and leave comments under a user's post in this blog app. I
         write about market conditions in the business category. Check it
         out!"
-        githubURL='https://github.com/alex-lvl/blog-api'
-        demoURL='https://alex-lvl.github.io/blog-react/'
+        githubURL='https://github.com/prosperitty/blog-api'
+        demoURL='https://prosperitty.github.io/blog-react/'
       >
         <Skill
           name='react'
@@ -144,7 +144,7 @@ function Projects(props) {
         as a back-end. Users may create todos and view all of them in their
         inbox or sorted by week, present, or project. Todos are saved with a
         Google account or on the local machine.'
-        githubURL='https://github.com/alex-lvl/todo-list'
+        githubURL='https://github.com/prosperitty/todo-list'
         demoURL='https://todo-list-c28c1.web.app/'
       >
         <Skill
@@ -177,8 +177,8 @@ function Projects(props) {
         description='View the weather forecast for a city in this weather application
         created with the Open Weather API and Javascript. Try searching for
         your city to fetch weather forecast details!'
-        githubURL='https://github.com/alex-lvl/weather-app'
-        demoURL='https://alex-lvl.github.io/weather-app/'
+        githubURL='https://github.com/prosperitty/weather-app'
+        demoURL='https://prosperitty.github.io/weather-app/'
       >
         <Skill
           name='javascript'
@@ -199,8 +199,8 @@ function Projects(props) {
         alt='computer world app preview'
         description='A simple, visually appealing front-end focused project created with
         react. Developed as an e-commerce styled website.'
-        githubURL='https://github.com/alex-lvl/shopping-cart'
-        demoURL='https://alex-lvl.github.io/shopping-cart/'
+        githubURL='https://github.com/prosperitty/shopping-cart'
+        demoURL='https://prosperitty.github.io/shopping-cart/'
       >
         <Skill
           name='react'
@@ -215,8 +215,8 @@ function Projects(props) {
         alt='tic tac toe app preview'
         description='Try defeating the unbeatable AI in this Tic-Tac-Toe game. Developed
         with Javascript and minmax algorithm.'
-        githubURL='https://github.com/alex-lvl/Tic-Tac-Toe'
-        demoURL='https://alex-lvl.github.io/Tic-Tac-Toe/'
+        githubURL='https://github.com/prosperitty/Tic-Tac-Toe'
+        demoURL='https://prosperitty.github.io/Tic-Tac-Toe/'
       >
         <Skill
           name='javascript'
@@ -233,7 +233,7 @@ function Projects(props) {
         description="Find all the listed characters in this where's waldo styled game!
         Post timed results to the leaderboard and compare your results to
         other players on the leaderboard page."
-        githubURL='https://github.com/alex-lvl/photo-tagging-app'
+        githubURL='https://github.com/prosperitty/photo-tagging-app'
         demoURL='https://photo-tagging-3a70f.web.app/'
       >
         <Skill
@@ -258,8 +258,8 @@ function Projects(props) {
         title='Battleship'
         alt='battleship app preview'
         description='Play a classic game of battleship against an AI.'
-        githubURL='https://github.com/alex-lvl/Battleship'
-        demoURL='https://alex-lvl.github.io/Battleship/'
+        githubURL='https://github.com/prosperitty/Battleship'
+        demoURL='https://prosperitty.github.io/Battleship/'
       >
         <Skill
           name='javascript'
@@ -279,8 +279,8 @@ function Projects(props) {
         title='Youtube Clone'
         alt='youtube clone project preview preview'
         description='A responsive design focused youtube clone project.'
-        githubURL='https://github.com/alex-lvl/youtube-page-clone'
-        demoURL='https://alex-lvl.github.io/youtube-page-clone/'
+        githubURL='https://github.com/prosperitty/youtube-page-clone'
+        demoURL='https://prosperitty.github.io/youtube-page-clone/'
       >
         <Skill name='HTML' class='project-stack-icon' vector={props.htmlIcon} />
         <Skill name='CSS' class='project-stack-icon' vector={props.cssIcon} />
@@ -294,7 +294,7 @@ function Projects(props) {
         available games in the store and how many copies are available for
         rent. Users may also view available games by category and see a list
         of games in the category.'
-        githubURL='https://github.com/alex-lvl/inventory-application'
+        githubURL='https://github.com/prosperitty/inventory-application'
         demoURL='https://inventory-application-6lsp.onrender.com/'
       >
         <Skill
@@ -326,8 +326,8 @@ function Projects(props) {
         alt='restaurant page project preview'
         description='A responsive design focused app. Developed with DOM manipulation.
         View the home page, menu, or contact details.'
-        githubURL='https://github.com/alex-lvl/restaurant-page'
-        demoURL='https://alex-lvl.github.io/restaurant-page/'
+        githubURL='https://github.com/prosperitty/restaurant-page'
+        demoURL='https://prosperitty.github.io/restaurant-page/'
       >
         <Skill
           name='javascript'
@@ -352,7 +352,7 @@ function Projects(props) {
         alt='members only app preview'
         description="An authentication focused app where users may post a message to the
         message board only if they're signed in."
-        githubURL='https://github.com/alex-lvl/members-only'
+        githubURL='https://github.com/prosperitty/members-only'
         demoURL='https://members-only-u9je.onrender.com/'
       >
         <Skill
@@ -378,8 +378,8 @@ function Projects(props) {
         title='Calculator'
         alt='calculator app preview'
         description='A calculator app that does basic math.'
-        githubURL='https://github.com/alex-lvl/simple-calculator'
-        demoURL='https://alex-lvl.github.io/simple-calculator/'
+        githubURL='https://github.com/prosperitty/simple-calculator'
+        demoURL='https://prosperitty.github.io/simple-calculator/'
       >
         <Skill
           name='javascript'
@@ -394,8 +394,8 @@ function Projects(props) {
         alt='newsweek clone project preview'
         description='A cloned version of the newsweek homepage using the Bootstrap
         library.'
-        githubURL='https://github.com/alex-lvl/newsweek-homepage'
-        demoURL='https://alex-lvl.github.io/newsweek-homepage/'
+        githubURL='https://github.com/prosperitty/newsweek-homepage'
+        demoURL='https://prosperitty.github.io/newsweek-homepage/'
       >
         <Skill
           name='bootstrap'
